e13b47be27ee80f6c63c856cf9cbf7c5ca47b15b
czuniga28 <chrisz.28052003@gmail.com>
2025-08-24 13:35:03 -0600

    Laboratorio 3 commit final entrega ok super duper

---
+++ Laboratorio3_Christopher_C28730/frontend-lab/src/App.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <CountriesList />
+</template>
+<script>
+import CountriesList from './components/CountriesList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CountriesList,
+  }
+};
+</script>
+
+<style>
+#app {
+
+}
+</style>
