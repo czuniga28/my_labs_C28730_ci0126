e8f6fc2d7a0334c7a115fa7bb6d5d7ca5db4f784
czuniga28 <chrisz.28052003@gmail.com>
2025-12-06 12:24:49 -0600

    Registrando repositorios y servicios

--- examen2-main/ExamTwo/ExamTwo/Program.cs
+++ examen2-main/ExamTwo/ExamTwo/Program.cs
@@ -1,4 +1,6 @@
 using ExamTwo.Controllers;
+using ExamTwo.Repositories;
+using ExamTwo.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register repositories
+builder.Services.AddSingleton<ICoffeeRepository, CoffeeRepository>();
+builder.Services.AddSingleton<IChangeRepository, ChangeRepository>();
+
+// Register services
+builder.Services.AddScoped<ICoffeeMachineService, CoffeeMachineService>();
 
 builder.Services.AddSingleton<Database>();
 
